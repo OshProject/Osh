170982d9547c8997796c9d4b7a5d975574e6319c
Administrator <Administrator@HW-SH-L-000008>
2016-02-25 10:48:10 +0800

    print "Hello World"

---
+++ src/com/MyGitHubService.java
@@ -0,0 +1,23 @@
+/**
+ * @File Name MyGitHubService.java
+ * @Package Name com
+ * @createDate 2016年2月25日上午10:42:27
+ * @author Tomorrow
+ * @Company hongware
+ * Copyright (c) 2016, hongware, Corp.All Rights Reserved.
+ *
+*/
+
+package com;
+/**
+ * @ClassName MyGitHubService 
+ * @description
+ * @createDate 2016年2月25日 上午10:42:27 
+ * @author Tomorrow
+ * @version 1.0
+ */
+public class MyGitHubService {
+
+}
+
+	
